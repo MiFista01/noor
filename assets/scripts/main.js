01c35f1e8b19ead95771df1aa376945847365937
MiFista01 <aleksei22891@gmail.com>
2022-11-10 06:57:40 +0200

    am,sd

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,49 +1,64 @@
 $(document).ready(function () {
-    let page = 0
+    var page = 0
     for(let i of $(".wrapper").children()){
         if(i.className !="button_down"){
             $(i).slideUp(0);
         }
     }
     $(".home").slideDown(0);
-    console.log($(".wrapper").children())
+    $("footer").slideDown(0);
     $("#button_down").click(function (e) { 
         e.preventDefault();
-        for(let i = 0; i <$(".wrapper").children().length-2; i++){
-            if(i == page){
-                ($(".wrapper").children()[i].className !="button_down")
-                $($(".wrapper").children()[i]).slideUp(200, function(){
-                    $($(".wrapper").children()[i+1]).slideDown(200);
-                    $(".button_up").show();
-                    page += 1
-                    if(page == $(".wrapper").children().length-3){
-                        $(".button_down").hide();
-                    }
-                });
+        $("div[page='"+page+"']").slideUp(200,()=>{
+            page ++;
+            $("div[page='"+page+"']").slideDown(200)
+            if(page == 0){
+                $(".button_up").hide(100);
+            }else if (page == $(".wrapper").children().length-3) {
+                $(".button_down").hide(100);
+            }else{
+                $(".button_down").show(100);
+                $(".button_up").show(100);
             }
-        }
+        });
+        
     });
     $("#button_up").click(function (e) { 
         e.preventDefault();
-        for(let i = 0; i <$(".wrapper").children().length-2; i++){
-            if(i == page){
-                ($(".wrapper").children()[i].className !="button_down")
-                $($(".wrapper").children()[i]).slideUp(200, function(){
-                    $($(".wrapper").children()[i-1]).slideDown(200);
-                    $(".button_up").show();
-                    page -= 1
-                    if(page == 0){
-                        $(".button_up").hide();
-                    }
-                    else{
-                        $(".button_down").show();
-                    }
-                });
+        $("div[page='"+page+"']").slideUp(200,()=>{
+            page --;
+            $("div[page='"+page+"']").slideDown(200)
+            if(page == 0){
+                $(".button_up").hide(100);
+            }else if (page == $(".wrapper").children().length-3) {
+                $(".button_down").hide(100);
+            }else{
+                $(".button_down").show(100);
+                $(".button_up").show(100);
             }
-        }
+        });
     });
     $(".ques").click(function (e) { 
         e.preventDefault();
         $($(e.target).next()).toggle(500);
+    });
+    $(".pages").click(function (e) { 
+        e.preventDefault();
+        let click_page = $(e.target).attr("page");
+        $("div[page='"+page+"']").slideUp(200,()=>{
+            page = click_page;
+            $("div[page='"+click_page+"']").slideDown(200)
+            if(page == 0){
+                $(".button_up").hide(100);
+                $(".button_down").show(100);
+            }else if (page == $(".wrapper").children().length-3) {
+                $(".button_down").hide(100);
+                $(".button_up").show(100);
+            }else{
+                $(".button_down").show(100);
+                $(".button_up").show(100);
+            }
+        });
+
     });
 });
